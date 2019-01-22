8bc0a36c790b0385a843331a7fa683254e1c546c
Aaron Baw <aaronbaw@gmail.com>
2019-01-22 09:34:11 +0000

    Reduce IOU threshold for resolving text, due to smaller bounding boxes returned by Google Cloud Vision

--- detection/src/textDetect.py
+++ detection/src/textDetect.py
@@ -50,4 +50,13 @@ def detectTextFromImage(image, ext='.png'):
     return detections
 
 if __name__ == "__main__":
-    print(detectTextFromImage(cv2.imread('images/benchmark.png')))
+    image = cv2.imread('clf/PrimitiveDetect/data/images/Scans-04.png')
+    preds = detectTextFromImage(image)
+    # Draw results on the image and write it.
+    for (text, bounding_box) in preds[1:]:
+        pt1 = tuple(bounding_box[0])
+        pt2 = tuple(bounding_box[2])
+        print(pt1, pt2)
+        cv2.rectangle(image, tuple(bounding_box[0]), tuple(bounding_box[2]), (255,0,0), 2)
+
+    cv2.imwrite('text_preds.png', image)
