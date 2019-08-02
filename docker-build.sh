4c21f6f0714df9908999bac407e2a0b557973a77
Aaron Baw <aaronbaw@gmail.com>
2019-08-02 20:43:27 +0200

    Fix Docker file & deploy script to use relative path dir from context

---
+++ docker-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Move the Google application credentials to the current dir before creating the
+# build context.
+mkdir .temp_creds
+cp $GOOGLE_APPLICATION_CREDENTIALS ./.temp_creds/google-cloud-compute-engine-key.json
+
+echo "Copied Google Cloud credentials to ./temp_creds."
+
+docker build -t aaronbaw/crimson-detection -f ./Dockerfile ./
+
+echo "Cleaning Google Cloud credentials."
+
+rm -rf ./.temp_creds/
