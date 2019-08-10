24d34ced8492c5a8ef2ce1fd54fda692014b44af
Aaron Baw <aaronbaw@gmail.com>
2019-08-10 22:23:09 +0200

    Refactor detectPrimitive module to choose between std i/o and web api

--- inference/modules/detectPrimitives.js
+++ inference/modules/detectPrimitives.js
@@ -4,13 +4,15 @@
 
 // Dependencies.
 const NodePyInt = require('./nodePyInt');
-var { resolve } = require('path');
+const request = require('request');
+const path = require('path');
 
 // Configuration.
 const _DETECT_SCRIPT_PATH = "../../detection/src/shapeDetect.py";
+const _WEB_API_ENDPOINT = process.env.SHAPE_DETECT_WEB_API_ENDPOINT || false;
 
-module.exports = async (imagePath) => {
-
+// Performs shape detection using standard streams.
+const detectViaStandardStream = async imagePath => {
   // Resolve path relative to root dir so that it can be interpreted correctly
   // by the python script.
   // imagePath = resolve(__dirname, imagePath);
@@ -19,29 +21,54 @@ module.exports = async (imagePath) => {
 
   // Instantiate the Node-Python-Interface.
   var detectShapes = NodePyInt(
-    resolve(__dirname,_DETECT_SCRIPT_PATH),
+    path.resolve(__dirname,_DETECT_SCRIPT_PATH),
     ['--image', imagePath], {
       pythonCmd : 'python3'
     });
 
   // Initialise analysis and return result as a JSON object.
   var output = {};
-  var result;
 
   try {
-    result = await detectShapes();
+    output = await detectShapes();
   } catch(e){
     log("Error detecting shapes: ", e);
   }
 
-  try {
-    output = JSON.parse(result);
-  } catch(e){
-    log("Error converting response to json:", result);
-  }
-
   return output;
 }
+
+// Performs shape detection by interfacing through a RESTful endpoint.
+const detectViaAPI = imagePath => new Promise((resolve, reject) => {
+    request({
+      method: 'POST',
+      url: _WEB_API_ENDPOINT,
+      headers:
+       { 'Postman-Token': '92d241b7-a0ea-4308-aab8-50e955ff62d0',
+         'cache-control': 'no-cache',
+         'Content-Type': 'application/x-www-form-urlencoded',
+         'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
+      formData: {
+         Image: {
+            value: fs.createReadStream(path.resolve(imagePath)),
+            options:
+             {
+               filename: imagePath,
+               contentType: null
+             }
+           }
+        }
+    }, (err, res, body) => {
+      if (err) return reject(err);
+      return resolve(body);
+    })
+});
+
+// Depending on whether the _WEB_API_ENDPOINT is set (indicating a server is running
+// and ready to receive requests to construct a shape hierarchy), choose to detect
+// shapes via the web API, or standard streams.
+module.exports = async (imagePath) => _WEB_API_ENDPOINT ? await detectViaAPI(imagePath) : detectViaStandardStream(imagePath);
+
 
 // Utility functions.
 function log(...msg){
