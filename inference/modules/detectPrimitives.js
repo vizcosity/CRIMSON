22131c02a4ef218a0b15480f3d0c4cfe33c41e87
Aaron Baw <aaronbaw@gmail.com>
2019-08-14 16:13:31 +0200

    Fix communication via REST between containers

--- inference/modules/detectPrimitives.js
+++ inference/modules/detectPrimitives.js
@@ -50,6 +50,7 @@ const detectViaAPI = imagePath => new Promise((resolve, reject) => {
       }
     }, (err, res, body) => {
       if (err) return reject(err);
+      log(`Recieved`, body);
       return resolve(JSON.parse(body));
     })
 });
