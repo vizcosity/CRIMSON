92b8be7bc5bd01112170bb0cf2a8e0e47330e307
Aaron Baw <aaronbaw@gmail.com>
2019-08-14 16:13:31 +0200

    Fix communication via REST between containers

--- docker-run-web.sh
+++ docker-run-web.sh
@@ -1,1 +1,1 @@
-docker run -e SHAPE_DETECT_WEB_API_ENDPOINT=crimson-detect:5373 -e DEBUG=true -d -p 3000:3000 -p 3715:3715 --name crimson-web-core --network=crimson_nw aaronbaw/crimson-web-core
+docker run -e SHAPE_DETECT_WEB_API_ENDPOINT=http://crimson-detection:5373/api/v1/shapeDetect -e DEBUG=true -d -p 3000:3000 -p 3715:3715 --name crimson-web-core --network=crimson_nw aaronbaw/crimson-web-core
