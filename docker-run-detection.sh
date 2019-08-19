1d454455293cff9789869fd2605dc3e8081a340a
Aaron Baw <aaronbaw@gmail.com>
2019-08-19 21:09:59 +0200

    Update docker run scripts, prepare for host networking & dynamic dns

--- docker-run-detection.sh
+++ docker-run-detection.sh
@@ -1,1 +1,4 @@
-docker run -d -p 5373:5373 --name crimson-detection --network=crimson_nw aaronbaw/crimson-detection
+docker run \
+-e CRIMSON_GODADDY_API_KEY=${CRIMSON_GODADDY_API_KEY} \
+-e CRIMSON_GODADDY_API_SECRET=${CRIMSON_GODADDY_API_SECRET} \
+-d -p 5373:5373 --name crimson-detection --network=host aaronbaw/crimson-detection
--- docker-run-web.sh
+++ docker-run-web.sh
@@ -1,1 +1,6 @@
-docker run -e SHAPE_DETECT_WEB_API_ENDPOINT=http://crimson-detection:5373/api/v1/shapeDetect -e DEBUG=true -d -p 3000:3000 -p 3715:3715 --name crimson-web-core --network=crimson_nw aaronbaw/crimson-web-core
+docker run \
+-e SHAPE_DETECT_WEB_API_ENDPOINT=http://crimson-detection.ventr.co.uk:5373/api/v1/shapeDetect \
+-e DEBUG=true \
+-e CRIMSON_GODADDY_API_KEY=${CRIMSON_GODADDY_API_KEY} \
+-e CRIMSON_GODADDY_API_SECRET=${CRIMSON_GODADDY_API_SECRET} \
+-d -p 3000:3000 -p 3715:3715 --name crimson-web-core --network=host aaronbaw/crimson-web-core
