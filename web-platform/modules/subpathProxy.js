532449d23ca662f8328bb8ba4ae5ff21d3a84d6d
Aaron Baw <aaronbaw@gmail.com>
2019-11-04 20:45:03 +0000

    Fully implement subpath proxying, fix unwanted display of ID numbers

--- web-platform/modules/subpathProxy.js
+++ web-platform/modules/subpathProxy.js
@@ -15,12 +15,36 @@ const cheerio = require('cheerio');
 // Takes in the subpath where the contained app will exist, and re-writes all
 // <a> tag href attributes so that they are prefixed by this subpath.
 const subspaceURLSForSubpath = (subpath, pageData) => {
+
   log(`Recieved proxy request.`);
+
+  // Ensure that we are parsing HTML and not other assets / media.
+  // We do this by ensuring that throughout the code, we have at least one
+  // set of opening and closing tags. Regex test credit: https://stackoverflow.com/a/15459273
+  var htmlTest = /<(br|basefont|hr|input|source|frame|param|area|meta|!--|col|link|option|base|img|wbr|!DOCTYPE).*?>|<(a|abbr|acronym|address|applet|article|aside|audio|b|bdi|bdo|big|blockquote|body|button|canvas|caption|center|cite|code|colgroup|command|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frameset|head|header|hgroup|h1|h2|h3|h4|h5|h6|html|i|iframe|ins|kbd|keygen|label|legend|li|map|mark|menu|meter|nav|noframes|noscript|object|ol|optgroup|output|p|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video).*?<\/\2>/i.test(pageData.toString('utf8'))
+
+  if (!htmlTest) return pageData;
+
+  // Replace all <a> tag 'href' attributes to be prefixed by subpath.
   var $ = cheerio.load(pageData);
   var aTags = $('a');
   for (var i = 0; i < aTags.length; i++){
     $(aTags[i]).attr('href', `/${subpath}${$(aTags[i]).attr('href')}`);
   }
+
+  // Replace all <link> tags, which referece CSS assets, so that the local
+  // files which are referenced are prefied by the subpath and are accessed
+  // appropriately when proxied.
+  var linkTags = $('link');
+
+  // Find link tags which are *not* urls (referencing local files) and prefix
+  // the 'src' attributes with the appropriate subpath prefix.
+  for (var i = 0; i < linkTags.length; i++){
+    var linkTag = $(linkTags[i]);
+    var currentHref = linkTag.attr('href');
+    linkTag.attr('href', currentHref.indexOf('http') === -1 ? `/${subpath}/${currentHref}` : currentHref);
+  }
+
   return $.html();
 }
 
