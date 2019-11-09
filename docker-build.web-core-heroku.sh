b493dcda7f7da58e916bcd98b05cd48d2761f83c
Aaron Baw <aaronbaw@gmail.com>
2019-11-09 13:14:15 +0000

    Add heroku env variables in a single command

--- docker-build.web-core-heroku.sh
+++ docker-build.web-core-heroku.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Enables separate dockerignore files for each container in the same repo.
+export DOCKER_BUILDKIT=1
+
 # Builds the docker image for the web platform pipeline with heroku containers.
 
 ln -s Dockerfile_web_core Dockerfile
@@ -7,9 +10,11 @@ ln -s Dockerfile_web_core Dockerfile
 ln -s Dockerfile_web_core.dockerignore .dockerignore
 
 # Set the GODADDY API CREDENTIAL ENV VARIABLES
-heroku config:set CRIMSON_GODADDY_API_KEY=$CRIMSON_GODADDY_API_KEY --app crimson-web-core
-heroku config:set CRIMSON_GODADDY_API_SECRET=$CRIMSON_GODADDY_API_SECRET --app crimson-web-core
-heroku config:set SHAPE_DETECT_WEB_API_ENDPOINT=$SHAPE_DETECT_WEB_API_ENDPOINT --app crimson-web-core
+heroku config:set \
+CRIMSON_GODADDY_API_KEY=$CRIMSON_GODADDY_API_KEY \
+CRIMSON_GODADDY_API_SECRET=$CRIMSON_GODADDY_API_SECRET \
+SHAPE_DETECT_WEB_API_ENDPOINT=$SHAPE_DETECT_WEB_API_ENDPOINT \
+--app crimson-web-core
 
 heroku container:push web --app crimson-web-core --remote heroku-web-core
 
