88a4f5501557c9f89917b5dc2fb5df4f26160adf
Aaron Baw <aaronbaw@gmail.com>
2020-03-22 15:40:33 +0100

    Add debug mode displaying mouse coordinates (canvas & absolute), fix incorrect positioning and size of newly creating primitives via dragging

--- web-platform/frontend/src/Toolbar.js
+++ web-platform/frontend/src/Toolbar.js
@@ -70,7 +70,7 @@ export default class Toolbar extends Component {
               this.selectButton("Select") |
               (this.props.selectButtonHandler && this.props.selectButtonHandler())
             }
-            className={this.state.selectedButton == "Select" ? "selected-button" : ""}
+            className={this.state.selectedButton === "Select" ? "selected-button" : ""}
           >
             <SelectPrimitiveIcon />
           </button>
@@ -82,7 +82,7 @@ export default class Toolbar extends Component {
               this.selectButton("Add") |
               (this.props.addPrimitiveHandler && this.props.addPrimitiveHandler())
             }
-            className={this.state.selectedButton == "Add" ? "selected-button" : ""}
+            className={this.state.selectedButton === "Add" ? "selected-button" : ""}
           >
             <AddPrimitiveIcon />
           </button>
@@ -94,7 +94,7 @@ export default class Toolbar extends Component {
               this.selectButton("Duplicate") |
               (this.props.duplicatePrimitiveHandler && this.props.duplicatePrimitiveHandler())
             }
-            className={this.state.selectedButton == "Duplicate" ? "selected-button" : ""}
+            className={this.state.selectedButton === "Duplicate" ? "selected-button" : ""}
           >
             <DuplicatePrimitiveIcon />
           </button>
@@ -111,7 +111,7 @@ export default class Toolbar extends Component {
               this.selectButton("Generate") |
               (this.props.generateCodeHandler && this.props.generateCodeHandler())
             }
-            className={this.state.selectedButton == "Generate" ? "selected-button" : ""}
+            className={this.state.selectedButton === "Generate" ? "selected-button" : ""}
           >
             <GenerateCodeIcon />
           </button>
@@ -122,7 +122,7 @@ export default class Toolbar extends Component {
             onClick={() =>
               this.selectButton("Back") |
               (this.props.goBackHandler && this.props.goBackHandler())}
-            className={this.state.selectedButton == "Back" ? "selected-button" : ""}
+            className={this.state.selectedButton === "Back" ? "selected-button" : ""}
           >
             <BackIcon />
           </button>
@@ -134,12 +134,30 @@ export default class Toolbar extends Component {
               this.selectButton("Help") |
               (this.props.helpButtonHandler && this.props.helpButtonHandler())
             }
-            className={this.state.selectedButton == "Help" ? "selected-button" : ""}
+            className={this.state.selectedButton === "Help" ? "selected-button" : ""}
           >
             <HelpIcon />
           </button>
         </div>
       </div>
+
+      <p style={{
+        marginTop: '5px',
+        marginLeft: 'auto',
+        fontSize: '8px'
+      }} className="toolbar-hover-text">
+        {
+          this.props.debugMode && this.props.absoluteMouse ? 
+          `(${this.props.absoluteMouse.x}, ${this.props.absoluteMouse.y})`
+          : ""
+        }
+        |
+        {
+          this.props.debugMode && this.props.canvasMouse ? 
+          `(${this.props.canvasMouse.x}, ${this.props.canvasMouse.y})`
+          : ""
+        }
+      </p>
 
       </div>
     );
