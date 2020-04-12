c1f7811d5d43d32501078d2a697d798729a6e959
Aaron Baw <aaronbaw@gmail.com>
2020-04-12 17:51:31 +0200

    Edit containerisation scripts to detect if running locally on Mac, in order to use Bridge network instead of Host network

--- docker-run-detection.sh
+++ docker-run-detection.sh
@@ -6,7 +6,16 @@
 # The following does not use the host network bridge, as this is not compatible
 # within MacOS.
 
+PLATFORM = $(uname)
+
+NETWORK = "host"
+
+if [ "$PLATFORM" == "Darwin" ]; then 
+    NETWORK = "bridge"
+fi
+
 docker run \
+-it \
 -e CRIMSON_GODADDY_API_KEY=${CRIMSON_GODADDY_API_KEY} \
 -e CRIMSON_GODADDY_API_SECRET=${CRIMSON_GODADDY_API_SECRET} \
--p 5373:5373 --name crimson-detection --network=crimson_bridge aaronbaw/crimson-detection
+-p 5373:5373 --name crimson-detection --network=${NETWORK} aaronbaw/crimson-detection
--- docker-run-web.sh
+++ docker-run-web.sh
@@ -9,9 +9,18 @@
 # The following does not use the host network bridge, as this is not compatible
 # within MacOS.
 
+PLATFORM=$(uname)
+
+NETWORK="host"
+
+if [ "$PLATFORM" == "Darwin" ]; then 
+    NETWORK = "bridge"
+fi
+
 docker run \
+-it \
 -e SHAPE_DETECT_WEB_API_ENDPOINT=https://crimson-detection.herokuapp.com/api/v1/shapeDetect \
 -e DEBUG=true \
 -e CRIMSON_GODADDY_API_KEY=${CRIMSON_GODADDY_API_KEY} \
 -e CRIMSON_GODADDY_API_SECRET=${CRIMSON_GODADDY_API_SECRET} \
--p 3000:3000 -p 3715:3715 --name crimson-web-core --network=crimson_bridge aaronbaw/crimson-web-core
+-p 3000:3000 -p 3715:3715 --name crimson-web-core --network=${NETWORK} aaronbaw/crimson-web-core
